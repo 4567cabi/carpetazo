3696b606253d914a6326ce20f14b08070326c27e
Damian <paluzdam@gmail.com>
2023-08-09 00:09:40 -0300

    mejoras para busquedas futuras

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
+function buscarPalabras() {
+    let resultado = '%20%28';
+    for (let i = 0; i < arguments.length; i++) {
+        if (resultado !== '%20%28') {
+            resultado += '%20OR%20';
+        }
+        resultado += arguments[i];
+    }
+
+    console.log(resultado);
+    return resultado.concat('%29');
+}
+
 function buscarTweets() {
     const twitterUsername = document.getElementById('twitterUsername').value
     const opcion = document.getElementById('filtros')
+    const optionError = document.getElementById('optionError');
+    const twitterSearch = "https://twitter.com/search?q=from%3A";
+    let searchQuery = twitterSearch + `${twitterUsername}`;
 
     if (twitterUsername.length > 0) {
         if (opcion.value == "machistas") {
-            const searchQuery = `https://twitter.com/search?q=from%3A${twitterUsername}%20%28mujer%20OR%20mujeres%20OR%20putas%29`
+            searchQuery += buscarPalabras('mujer', 'mujeres', 'putas');
             window.open(searchQuery, '_blank')
         } else if (opcion.value == "homofob") {
-            const searchQuery = `https://twitter.com/search?q=from%3A${twitterUsername}%20%28gay%20OR%20gays%20OR%20putos%20OR%20trolos%29`
+            searchQuery += buscarPalabras('gay', 'gays', 'putos', 'trolos');
             window.open(searchQuery, '_blank')
         } else if (opcion.value == "racistas") {
-            const searchQuery = `https://twitter.com/search?q=from%3A${twitterUsername}%20%28negro%20OR%20negros%20OR%20marron%20OR%20marrones%20OR%20nword%29`
+            searchQuery += buscarPalabras('negro', 'negros', 'marron', 'marrones', 'nword');
             window.open(searchQuery, '_blank')
         } else if (opcion.value == "messi") {
-            const searchQuery = `https://twitter.com/search?q=from%3A${twitterUsername}%20messi%20until%3A2021-01-01`
-            window.open(searchQuery, '_blank')
+            const messiQuery = searchQuery + `%20messi%20until%3A2021-01-01`
+            window.open(messiQuery, '_blank')
         } else if (opcion.value == "electoral") {
-            const searchQuery = `https://twitter.com/search?q=from%3A${twitterUsername}%20%28milei%20OR%20massa%20OR%20larreta%20OR%20bullrich%29`
+            searchQuery += buscarPalabras('milei', 'massa', 'larreta', 'bullrich');
             window.open(searchQuery, '_blank')
         } else if (opcion.value == "seleccione") {
-            alert("Seleccioná una opción para el carpetazo")
+            optionError.innerHTML = 'Ingrese una opción válida!!!';
+            optionError.style.display = "block";
+
+            // reiniciamos el texto luego de 2 segundos
+            setTimeout(() => {
+                optionError.innerHTML = '';
+                divCopiar.style.display = "none";
+            }, 2000);
         }
     } else {
-        alert("Poné un usuario")
+        optionError.innerHTML = 'Ingrese un usuario valido!!!';
+        optionError.style.display = "block";
+        // reiniciamos el texto
+        setTimeout(() => {
+            optionError.innerHTML = '';
+            divCopiar.style.display = "none";
+        }, 3000);
     }
+
 }
 
 
